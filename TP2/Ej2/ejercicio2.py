8d0a954b99da82ce1a4ced38607a087ed0e3be82
John Doe <johndoe@example.com>
2016-10-26 10:07:31 -0300

    falta importar

--- TP2/Ej2/ejercicio2.py
+++ TP2/Ej2/ejercicio2.py
@@ -1,167 +1,215 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
-N = 856
-#N = 4
-M1 = 20
-M2 = 20
-M = M1 * M2
-radio = 15
-coef_aprendizaje_const = 0.999
+import csv, sys, math, os
 
 
-def preprocesamiento(datos):
-    datos[:,1:] = (datos[:,1:] - np.median(datos[:,1:])) / np.std(datos[:,1:])
-    return
+class kohonen:
+    def __init__(self):
+        #--------------------------------------------
+        #------Parametros
+        self.learning_rate = 0.999
+        self.tolerancia_error = 0.00001
+        self.cantidad_epocas = 100000
+        self.dimension = 20
+        self.entradas = 200
+        self.input_file = "../tp2_training_dataset.csv" #--archivo csv de entrada
+
+        #--------------------------------------------
+        self.N = 856
+        self.M1 = self.dimension
+        self.M2 = self.dimension
+        self.M = self.M1 * self.M2
+        self.W = np.random.random_sample((self.M,self.N))
+        self.dW = np.zeros((self.M,self.N))
+        self.y = np.zeros((self.M1,self.M2))
+        self.x = np.zeros((1,self.N))
+        self.cant_categorias = 9
 
 
-filename = "../tp2_training_dataset.csv"
-#filename = "prueba.csv"
+    def entrenar(self):
+		num = 1
+		entrada = np.genfromtxt(self.input_file, delimiter=',')
+		datos_entrenamiento = entrada[0:self.entradas,:]
 
-entrada = np.genfromtxt(filename, delimiter=',')
-datos_entrenamiento = entrada[0:100,:]
-#datos_entrenamiento = np.array([entrada[:]])
-#datos_entrenamiento[:,1:N+1] = datos_entrenamiento[:,1:N+1] / 10
-#preprocesamiento(datos_entrenamiento)
+		epoca_actual = 1
+		norma_actual = 0
+		norma_anterior = 0
+		diferencia_norma = 100
 
+		while epoca_actual < self.cantidad_epocas and diferencia_norma > self.tolerancia_error:
+			print "epoca:" +  str(epoca_actual)
+			print "diferencia:" +  str(diferencia_norma)
+			matriz_resultados = np.zeros((self.M1,self.M2,self.cant_categorias))
+			matriz_colores = np.zeros((self.M1, self.M2))
+			sigma_o = self.M2/2
+			lambd = float(self.cantidad_epocas*len(datos_entrenamiento))/math.log(sigma_o)
 
-W = np.random.random_sample((M,N))
-dW = np.zeros((M,N))
-y = np.zeros((M1,M2))
-x = np.zeros((1,N))
-cant_categorias = 9
+			for fila in datos_entrenamiento:
+				# va bien
+				#sigma = sigma_o * math.exp(-float(epoca_actual)/lambd)
+				#coef_aprendizaje = self.learning_rate * math.exp(-float(epoca_actual)/lambd)
 
+				sigma = sigma_o * math.exp(-float(num)/lambd)
+				#coef_aprendizaje = self.learning_rate * math.exp(-float(num)/lambd)
+				
+				#sigma = sigma_o * 1 / (float(epoca_actual)**(1/3) * (1/float(self.cantidad_epocas*len(datos_entrenamiento))*1/4 / 1/4) + 1)
+				coef_aprendizaje = self.learning_rate * 0.9
+				self.dW[:] = np.zeros((self.M,self.N))
+				self.x[:] = np.array([fila[1:self.N+1]])
+				categoria = fila[0]-1
+				yp = np.zeros((self.M,1))
+				yp.T[:] = np.array([np.sum(np.absolute(self.x - self.W), axis=-1)])
+				self.y = ((yp == np.min(yp))*1).reshape(self.M1,self.M2)
+				ganadora = np.nonzero(self.y)
+				matriz_resultados[ganadora[0][0],ganadora[1][0],categoria] += 1
 
+				#...........bmu..............
+				D = np.zeros((self.M))
+				for i in xrange(0,self.M1):
+					for j in xrange(0,self.M2):
+						suma = 0
+						suma += (i-ganadora[0][0])**2
+						suma += (j-ganadora[1][0])**2
+						if suma < (sigma**2):
+							D[(i * self.M2) + j] = math.exp(- suma / (2 * sigma**2))
+							self.dW[(i * self.M2) + j,:] = (coef_aprendizaje * D[(i * self.M2) + j] * (self.x - self.W[(i * self.M2) + j,:])).reshape(self.N)
 
-num_etapa = 1
-num = 1
-maximo_etapas = 500
-while num_etapa < maximo_etapas:
-	print "epoca:" + str(num_etapa)
-	num_fila = 0
-	#coef_aprendizaje = 1/(float(num_etapa))
-	#sigma = (float(M2)/2) * 1/(float(num_etapa**3))
-	#sigma = (float(M2)/2) * 1/(float(num_etapa))
-	#sigma_ratio = float(num_etapa) / math.log(float(M2)/2)
-	#sigma = (float(M2)/2) * math.exp(-(float(num_etapa-1)/sigma_ratio))
-	#print "------sigma: "+str(sigma)
-	
-	#sigma_ratio = 1 / float(num_etapa)
-	#sigma = (M2/2) * math.exp(-4*num_etapa/maximo_etapas)
-	#sigma = (M2/2) * (float(maximo_etapas-num_etapa))/maximo_etapas
+				self.W += self.dW
+				num += 1
 
-#	sigma_o = M2/2
-#	lambd = float(maximo_etapas)/math.log(sigma_o)
-#	sigma = sigma_o * math.exp(-float(num_etapa)/lambd)
-#	coef_aprendizaje = coef_aprendizaje_const * math.exp(-float(num_etapa)/lambd)
-	
-	matriz_resultados = np.zeros((M1,M2,cant_categorias))
-	matriz_colores = np.zeros((M1, M2))
+			norma_actual = np.linalg.norm(self.W)
+			diferencia_norma = abs(norma_actual - norma_anterior)
+			norma_anterior = norma_actual
 
-	sigma_o = M2/2
+			epoca_actual += 1
 
-	lambd = float(maximo_etapas*len(datos_entrenamiento))/math.log(sigma_o)
-	for fila in datos_entrenamiento:
-		
-		#sigma = sigma_o * math.exp(-float(num)/lambd)
-		#coef_aprendizaje = coef_aprendizaje_const * math.exp(-float(num)/lambd)
-		#sigma = sigma_o * 1 / (float(num_etapa)**(1/3) * (1/lambd) + 1)
-		#coef_aprendizaje = coef_aprendizaje_const * math.exp(-float(num)/lambd)
-		#sigma = sigma_o * 1 / ((float(num_etapa)**(1/3)))
-		#coef_aprendizaje = coef_aprendizaje_const * 0.95
-
-		sigma = sigma_o * 1 / (float(num_etapa)**(1/3) * (1/float(maximo_etapas*len(datos_entrenamiento))*1/4 / 1/4) + 1)
-		coef_aprendizaje = coef_aprendizaje_const * 0.9
-
-
-
-		dW[:] = np.zeros((M,N))
-		x[:] = np.array([fila[1:N+1]])
-
-		categoria = fila[0]-1
-
-		yp = np.zeros((M,1))
-
-		yp.T[:] = np.array([np.sum(np.absolute(x - W), axis=-1)])
-
-		y = ((yp == np.min(yp))*1).reshape(M1,M2)
-
-		ganadora = np.nonzero(y)
-
-		#if num_etapa == maximo_etapas-1:
-			#print str(ganadora[0][0])+", "+str(ganadora[1][0])+" -> "+str(categoria)
-		matriz_resultados[ganadora[0][0],ganadora[1][0],categoria] += 1
-
-		#print str(num_etapa)+" "+str(num_fila)+" -> "+str(ganadora[0][0])+", "+str(ganadora[1][0]) para optimizar
+			for i in xrange(0,self.M1):
+				for j in xrange(0,self.M2):
+					if matriz_resultados[i,j,np.argmax(matriz_resultados[i,j])] == 0:
+						matriz_colores[i,j] = 0
+					else :
+						matriz_colores[i,j] = np.argmax(matriz_resultados[i,j])+1
 
 
+			#if epoca_actual % 20 == 0: 
+				#plt.matshow(matriz_colores)
+				#plt.show()
+				#plt.savefig("mapa_"+str(epoca_actual)+".png")
 
-		#...........raro < j / m2, j mod m2 >..............
-#		P = np.zeros((M,2))
-#		for j in xrange(0,M):
-#			P[j,0] = j / M2
-#			P[j,1] = j % M2
-#
-#		j_ganadora = (ganadora[0][0] * M2) + ganadora[1][0]
-#
-#		D = np.zeros((M))
-#		for j in xrange(0,M):
-#			suma = 0
-#			suma += (P[j,0]-P[j_ganadora,0])**2
-#			suma += (P[j,1]-P[j_ganadora,1])**2
-#			D[j] = math.exp(- suma / (2 * sigma**2))
-#			dW[j,:] = (coef_aprendizaje * D[j] * (x - W[j,:])).reshape(N)
-
-
-		#...........bmu..............
-		D = np.zeros((M))
-		for i in xrange(0,M1):
-			for j in xrange(0,M2):
-				suma = 0
-				suma += (i-ganadora[0][0])**2
-				suma += (j-ganadora[1][0])**2
-				if suma < (sigma**2):
-					D[(i * M2) + j] = math.exp(- suma / (2 * sigma**2))
-					dW[(i * M2) + j,:] = (coef_aprendizaje * D[(i * M2) + j] * (x - W[(i * M2) + j,:])).reshape(N)
-
-
-		#...........escalones..............
-#		y_desde = ganadora[0][0]-radio
-#		y_hasta = ganadora[0][0]+radio
-#		x_desde = ganadora[1][0]-radio
-#		x_hasta = ganadora[1][0]+radio
-
-#		for i in xrange(y_desde,y_hasta+1):
-#			for j in xrange(x_desde,x_hasta+1):
-#				distancia = math.fabs(ganadora[0][0] - i)
-#				if distancia > math.fabs(ganadora[1][0] - j):
-#					distancia = math.fabs(ganadora[1][0] - j)
-#				dW[(i % M1) * M2 + (j % M2)] = coef_aprendizaje * (x - W[(i % M1) * M2 + (j % M2)]) * (1 - distancia/radio)
-				#print "---acomoda -> "+str(i % M1)+", "+str(j % M2)+" ->"+str((i % M1) * M2 + (j % M2))
-
-		W += dW
-
-		num_fila += 1
-		num += 1
-
-#	radio -= num_etapa % 3
-#	if radio < 1:
-#		radio = 1
-	num_etapa += 1
-
-	#print matriz_resultados
-
-	for i in xrange(0,M1):
-		for j in xrange(0,M2):
-			if matriz_resultados[i,j,np.argmax(matriz_resultados[i,j])] == 0:
-				matriz_colores[i,j] = 0
-			else :
-				matriz_colores[i,j] = np.argmax(matriz_resultados[i,j])+1
-
-	#print matriz_colores para optimizar
-
-	if num_etapa % 20 == 0: 
 		plt.matshow(matriz_colores)
 		#plt.show()
-		plt.savefig("mapa_"+str(num_etapa)+".png")
+		plt.savefig("mapa_"+str(epoca_actual)+".png")
+
+
+ 		return "Fin entrenamiento"
+
+    def cambiar_valor(self, clave, valor):
+        if clave == 'lr':
+            self.learning_rate = float(valor)
+        elif clave == 'tol':
+            self.tolerancia_error = float(valor)
+        elif clave == 'cep':
+            self.cantidad_epocas = float(valor)
+        elif clave == 'dim':
+            self.dimension = float(valor)
+        elif clave == 'cant':
+            self.entradas = float(valor)
+        elif clave == 'input':
+            self.input_file = valor
+
+
+    def importarW(self, filename):
+        csv_entrada = csv.reader(open(filename, "rb"), delimiter=',')
+        
+        return "TODO"
+
+
+    def exportarW(self, filename):
+        csv_salida = csv.writer(open(filename, "wb"))
+        csv_salida.writerow([self.W])
+        for i in xrange(0,len(self.W)):
+            csv_salida.writerow(self.W[i])
+       
+        return "Mapa exportado con exito!"
+
+
+    def testing(self):
+        #--busca datos del archivo de entrada
+
+        return "TODO"
+
+
+def mostrar_menu(koh, msg):
+    os.system('clear');
+    print "MENU DEL EJERCICIO2:\n"
+    print "Parametros activos:"
+    print "  - learning rate:             (lr)     " + str(koh.learning_rate)
+    print "  - tolerancia de error:       (tol)    " + str(koh.tolerancia_error)
+    print "  - cantidad epocas:           (cep)    " + str(koh.cantidad_epocas)
+    print "  - dimension del mapa:        (dim)    " + str(koh.dimension)
+    print "  - cantidad de datos (train): (cant)   " + str(koh.entradas)
+    print "  - input file (input) " + str(koh.input_file)
+    print "\n"
+    print "help -> para ver los comandos validos y su modo de uso"
+    print "exit -> terminar la ejecucion del programa\n"
+    if len(msg) > 0:
+        print "Respuesta:  "+str(msg)
+    else:
+        print ""
+    print ""
+
+
+
+def mostrar_ayuda():
+    os.system('clear');
+    print "AYUDA DEL EJERCICIO2:\n"
+    print "Acciones validas:\n"
+    print "  - Importar un mapa de Kohonen"
+    print "         Descripcion: importa un mapa desde un archivo"
+    print "         Uso: import nombre_archivo.extension"
+    print "         Ejemplo: import mapaOK.in"
+    print ""
+    print "  - Exportar un mapa"
+    print "         Descripcion: exporta el mapa actual hacia un archivo"
+    print "         Uso: export nombre_archivo.extension"
+    print "         Ejemplo: export mapa_train.in"
+    print ""
+    print "  - Cambiar parametro"
+    print "         Descripcion: modifica el valor de un parametro"
+    print "         Uso: change codigo_parametro nuevo_valor"
+    print "         Ejemplo: change lr 0.001"
+    print ""
+    print "  - Comenzar entrenamiento"
+    print "         Descripcion: ejecuta el entrenamiento con los parametros guardados"
+    print "         Uso: train"
+    print ""
+
+koh = kohonen()
+
+salir = 0
+msg = ""
+
+
+while (not salir):
+    mostrar_menu(koh, msg)
+    msg = ""
+    comando = raw_input("Ingrese un comando: ");
+    comando = comando.split(" ")
+    if (comando[0] == 'exit'):
+        salir = 1
+    elif (comando[0] == 'help'):
+        mostrar_ayuda()
+        raw_input("Pulse enter para volver al menu...")
+    elif (comando[0] == 'train'):
+        msg = koh.entrenar()
+        raw_input("Pulse enter para volver al menu...")
+    elif (comando[0] == 'change'):
+        koh.cambiar_valor(comando[1], comando[2])
+    elif (comando[0] == 'export'):
+        msg = koh.exportarW(comando[1])
+    elif (comando[0] == 'import'):
+        msg = koh.importarW(comando[1])
+    elif (comando[0] == 'test'):
+        koh.testing()
+        raw_input("Pulse enter para volver al menu...")
