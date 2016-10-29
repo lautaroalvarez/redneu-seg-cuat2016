df1143cf70f75e090b0af56057d5a3417985f8d5
laucha <laucha@laucha.laucha>
2016-10-29 01:59:42 -0300

    Export, import red en ej2

--- TP2/Ej2/ejercicio2.py
+++ TP2/Ej2/ejercicio2.py
@@ -15,6 +15,9 @@ class kohonen:
 		self.entradas = 300
 		self.input_file = "../tp2_training_dataset.csv" #--archivo csv de entrada
 
+		self.data_entrenamiento = np.empty((1,1))
+		self.data_validacion = np.empty((1,1))
+
 		#--------------------------------------------
 		self.N = 856
 		self.M1 = self.dimension
@@ -23,11 +26,25 @@ class kohonen:
 		self.W = np.random.random_sample((self.M,self.N))
 		self.cant_categorias = 9
 
+		self.actualizarDataSet()
+		
+		self.Mres = np.zeros((self.M1,self.M2)) #--resultado con las categorias asignadas
+
+
+	def actualizarDataSet(self):
+		#--lectura de archivo de entrada
+		data_input = np.genfromtxt(self.input_file, delimiter=',')
+		#--preprocesamiento
+		data_input[:,1:] = data_input[:,1:] / 10
+		
+		#--cambiamos el orden del dataset
+		np.random.shuffle(data_input)
+		self.data_entrenamiento = data_input[0:self.entradas,:]
+		self.data_validacion = data_input[self.entradas+1:,:]
+
 
 	def entrenar(self):
 		num = 1
-		entrada = np.genfromtxt(self.input_file, delimiter=',')
-		datos_entrenamiento = entrada[0:self.entradas,:]
 
 		epoca_actual = 1
 		norma_actual = 0
@@ -39,22 +56,19 @@ class kohonen:
 		x = np.zeros((1,self.N))
 		yp = np.zeros((self.M,1))
 
-#teeest
-		#matriz_resultados = np.zeros((self.M1,self.M2,self.cant_categorias))
-		#matriz_colores = np.zeros((self.M1, self.M2))
+		self.Mres[:] = 0;
 
 		while epoca_actual < self.cantidad_epocas and diferencia_norma > self.tolerancia_error:
 			print "epoca:" +  str(epoca_actual)
 			print "diferencia:" +  str(diferencia_norma)
 			matriz_resultados = np.zeros((self.M1,self.M2,self.cant_categorias))
-			matriz_colores = np.zeros((self.M1, self.M2))
 			sigma_o = self.M2/2
-			lambd = float(self.cantidad_epocas*len(datos_entrenamiento))/math.log(sigma_o)
+			lambd = float(self.cantidad_epocas*len(self.data_entrenamiento))/math.log(sigma_o)
 			coef_aprendizaje = epoca_actual ** (-1)
 			sigma = sigma_o * epoca_actual ** (-1.0/3.0)
 			coef_aprendizaje =self.learning_rate / (1 + epoca_actual * 0.5 * self.learning_rate)
 
-			for fila in datos_entrenamiento:
+			for fila in self.data_entrenamiento:
 				# va bien
 				#sigma = sigma_o * math.exp(-float(epoca_actual)/lambd)
 				#coef_aprendizaje = self.learning_rate * math.exp(-float(epoca_actual)/lambd)
@@ -62,18 +76,17 @@ class kohonen:
 				#sigma = sigma_o * math.exp(-float(num)/lambd)
 				#coef_aprendizaje = self.learning_rate * math.exp(-float(num)/lambd)
 				
-				#sigma = sigma_o * 1 / (float(epoca_actual)**(1/3) * (1/float(self.cantidad_epocas*len(datos_entrenamiento))*1/4 / 1/4) + 1)
+				#sigma = sigma_o * 1 / (float(epoca_actual)**(1/3) * (1/float(self.cantidad_epocas*len(self.data_entrenamiento))*1/4 / 1/4) + 1)
 				#coef_aprendizaje = self.learning_rate * 0.9
 				dW[:] = np.zeros((self.M,self.N))
 				x[:] = np.array([fila[1:self.N+1]])
 				categoria = fila[0]-1
-				#yp = np.zeros((self.M,1))
 				yp.T[:] = np.array([np.sum(np.absolute(x - self.W), axis=-1)])
 				y = ((yp == np.min(yp))*1).reshape(self.M1,self.M2)
 				ganadora = np.nonzero(y)
 				matriz_resultados[ganadora[0][0],ganadora[1][0],categoria] += 1
 
-				#...........bmu..............
+
 				D = np.zeros((self.M))
 				for i in xrange(0,self.M1):
 					for j in xrange(0,self.M2):
@@ -96,17 +109,17 @@ class kohonen:
 			for i in xrange(0,self.M1):
 				for j in xrange(0,self.M2):
 					if matriz_resultados[i,j,np.argmax(matriz_resultados[i,j])] == 0:
-						matriz_colores[i,j] = 0
+						self.Mres[i,j] = 0
 					else :
-						matriz_colores[i,j] = np.argmax(matriz_resultados[i,j])+1
+						self.Mres[i,j] = np.argmax(matriz_resultados[i,j])+1
 
 
 			if epoca_actual % 20 == 0: 
-				plt.matshow(matriz_colores)
+				plt.matshow(self.Mres)
 				#plt.show()
 				plt.savefig("mapa_"+str(epoca_actual)+".png")
 
-		plt.matshow(matriz_colores)
+		plt.matshow(self.Mres)
 		#plt.show()
 		plt.savefig("mapa_"+str(epoca_actual)+".png")
 
@@ -123,20 +136,23 @@ class kohonen:
 		elif clave == 'dim':
 			#--modifica dimensiones en matrices
 			tamano_viejo = self.dimension
-			self.dimension = float(valor)
-			if tamano_viejo < self.dimension:
-				self.W = np.append(self.W, np.random.randn(self.dimension - tamano_viejo, self.N), axis=1)
-			else:
-				self.W = self.W[0:self.dimension,0:self.dimension]
+			self.dimension = int(valor)
 			self.M1 = self.dimension
 			self.M2 = self.dimension
 			self.M = self.M1 * self.M2
+			if tamano_viejo < self.dimension:
+				self.W = np.append(self.W, np.random.randn(self.M - tamano_viejo**2, self.N), axis=1)
+				self.Mres = np.append(self.Mres, np.random.randn(self.dimension - tamano_viejo, self.N), axis=1)
+			else:
+				self.W = self.W[0:self.M,0:self.N]
+				self.Mres = self.Mres[0:self.dimension,0:self.dimension]
 		elif clave == 'cant':
 			self.entradas = float(valor)
 		elif clave == 'input':
 			self.input_file = valor
+			self.actualizarDataSet()
 
-	def importarW(self, filename):
+	def importarRed(self, filename):
 		csv_entrada = csv.reader(open(filename, "rb"), delimiter=',')
 		
 		cont = 0
@@ -145,30 +161,64 @@ class kohonen:
 				#--modifica dimensiones en matrices
 				tamano_viejo = self.dimension
 				self.dimension = int(row[0])
-				if tamano_viejo < self.dimension:
-					self.W = np.append(self.W, np.random.randn(self.dimension - tamano_viejo, self.N), axis=1)
-				elif tamano_viejo > self.dimension:
-					self.W = self.W[0:self.dimension,0:self.N]
 				self.M1 = self.dimension
 				self.M2 = self.dimension
 				self.M = self.M1 * self.M2
-			else:
+				if tamano_viejo < self.dimension:
+					self.W = np.append(self.W, np.random.randn(self.M - tamano_viejo**2, self.N), axis=1)
+					self.Mres = np.append(self.Mres, np.random.randn(self.dimension - tamano_viejo, self.N), axis=1)
+				else:
+					self.W = self.W[0:self.M,0:self.N]
+					self.Mres = self.Mres[0:self.dimension,0:self.dimension]
+				print self.W.shape
+				print self.Mres.shape
+			elif cont <= self.M:
+				print "entra a w: "+str(cont)
 				self.W[cont-1,:] = row
+			else:
+				print "entra a Mres: "+str(cont)+" -> "+str(cont-self.M)
+				self.Mres[cont-self.M-1,:] = row
 			cont += 1
 		return "Red importada con exito!"
 
-	def exportarW(self, filename):
+	def exportarRed(self, filename):
 		csv_salida = csv.writer(open(filename, "wb"))
 		csv_salida.writerow([self.dimension])
 		for fila in self.W:
+			csv_salida.writerow(fila)
+		for fila in self.Mres:
 			csv_salida.writerow(fila)
 		return "Red exportada con exito!"
 
 
 	def testing(self):
-		#--busca datos del archivo de entrada
+		cant_bien = 0
+		cant_vacio = 0
 
-		return "TODO"
+		y = np.zeros((self.M1,self.M2))
+		x = np.zeros((1,self.N))
+		yp = np.zeros((self.M,1))
+		
+		for fila in self.data_validacion:
+			x[:] = np.array([fila[1:self.N+1]])
+			categoria = fila[0]
+
+			yp.T[:] = np.array([np.sum(np.absolute(x - self.W), axis=-1)])
+			y = ((yp == np.min(yp))*1).reshape(self.M1,self.M2)
+			ganadora = np.nonzero(y)
+		
+			if self.Mres[ganadora[0][0]][ganadora[1][0]] == categoria:
+				#--cayo en su categoria
+				cant_bien += 1
+			elif self.Mres[ganadora[0][0]][ganadora[1][0]] == 0:
+				cant_vacio += 1
+
+		print "Resultado Validacion:"
+		print "      "+str(cant_bien)+" bien"
+		print "      "+str(cant_vacio)+" sin determinar"
+		print "      "+str(len(self.data_validacion)-cant_bien)+" mal"
+
+		return
 
 
 def mostrar_menu(koh, msg):
@@ -215,6 +265,10 @@ def mostrar_ayuda():
 	print "         Descripcion: ejecuta el entrenamiento con los parametros guardados"
 	print "         Uso: train"
 	print ""
+	print "  - Testear Red"
+	print "         Descripcion: testea la respuesta de la red a los datos de validacion"
+	print "         Uso: test"
+	print ""
 
 koh = kohonen()
 
@@ -238,9 +292,9 @@ while (not salir):
 	elif (comando[0] == 'change'):
 		koh.cambiar_valor(comando[1], comando[2])
 	elif (comando[0] == 'export'):
-		msg = koh.exportarW(comando[1])
+		msg = koh.exportarRed(comando[1])
 	elif (comando[0] == 'import'):
-		msg = koh.importarW(comando[1])
+		msg = koh.importarRed(comando[1])
 	elif (comando[0] == 'test'):
 		koh.testing()
 		raw_input("Pulse enter para volver al menu...")
